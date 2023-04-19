cea64765689646f996fbb2fafa2934479df610e5
=Matias Salomon <matias.salomon@clt.com.py>
2023-04-19 14:04:13 -0400

    Creacion de CountryController para desplegar pagina y hacer peticion GET y rellenar la tabla

---
+++ Section25-Identity/Section25-Identity/Controllers/CityController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Section25_Identity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+    }
+}
---
+++ Section25-Identity/Section25-Identity/Controllers/CountryController.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Section25_Identity.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CountryController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public CountryController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await context.Countries.ToListAsync();
+            return View(result);
+        }
+    }
+}
